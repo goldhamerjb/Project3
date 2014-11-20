dd987cfbdeae6713995387f47597d3e9d66496ee
twc1990 <twcontes@gmail.com>
2014-11-20 10:57:24 -0500

    Update importStudentInfo.java

--- importStudentInfo.java
+++ importStudentInfo.java
@@ -1,15 +1,17 @@
 /*
-created 11/9/2014
+created 11/19/2014
 Imports information describing a list of Students from a text file.
 Information for each student is scanned and the student is added to
  the StudentList.
+ 
+ edited 11/20/2014, added ability to read files containing complete student info(advising information as well as name/id/grade)
 */
 import java.util.Scanner;
 import java.io.*;
-
-
-public void importStudentInfo(File stuInfo)
-//Accepts a text file containing student information.
+public void importStudentInfo(){
+public void importStudentInfo(){}
+public void readNewStudentInfo(File stuInfo)
+//Accepts a text file containing student information that is not .
 {
 File studentInformation = stuInfo;
 Scanner infoReader = new Scanner(studentInformation);
@@ -38,5 +40,53 @@ while (infoLine.hasNext();
 	StudentList.add(new Student(newName, newID, newGrade));
 	//A student is added to the StudentList after each line is scanned.
 	}
-}
+}}
+
+public void readNewStudentInfo(File stuInfo)
+//Accepts a text file containing student information that is complete
+{
+File studentInformation = stuInfo;
+Scanner infoReader = new Scanner(studentInformation);
+//Creates scanner for text file.
+
+String infoLine = new String;
+Scanner parser = new Scanner(infoLine);
+//Creates scanner to parse strings from file.
+while (infoReader.hasNext())
+{
+String newName, newID, newGrade, newHasBeenAdvised, newAdvisingDate; //Variable to hold new info
+while (infoLine.hasNext();
+	{
+	infoLine = infoReader.nextLine();
+	parser.useDelimiter("&");
+	newName = parser.next();
+	newID = parser.next();
+	newGrade = parser.next();
+	newHasBeenAdvised = parser.next();
+	newAdvisingDate = parser.next();
+	int format=newHasBeenAdvised.Length();
+	char firstSymbol = newHasBeenAdvised.charAt(0);
+	int beenAdvised;
+	if ((format==1)}
+		{if (firstSymbol=='1')
+			{beenAdvised = 1;}
+		else 
+			{	beenAdvised = 0;
+				newAdvisingDate = "NaN";}
+		}
+	else
+		{beenAdvised = 0;
+		newAdvisingDate = "NaN";
+		}
+	/*
+	Parser assumes text file has the following format:
+	Each student's info is separated by a new line.
+	Each segment of a student's information is immediately
+	 followed by the "&" as a delimiter.
+	The order of the information is name, ID, grade, if they have been advised, date of advising.
+	*/
+	StudentList.add(new Student(newName, newID, newGrade, newHasBeenAdvised, AdvisingDate));
+	//A student is added to the StudentList after each line is scanned.
+	}
+}}
 }

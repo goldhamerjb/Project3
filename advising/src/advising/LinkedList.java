d223f685e5a63dffdb123274d699787e204e8183
twc1990 <twcontes@gmail.com>
2014-11-25 18:37:19 -0500

    Signed-off-by: twc1990 <twcontes@gmail.com>

--- advising/src/advising/LinkedList.java
+++ advising/src/advising/LinkedList.java
@@ -14,17 +14,34 @@ public class LinkedList<T extends Comparable<T>> {
 		length = 0;
 	}
 
+	public void ChangeData(T data, T data2, T data3)
+		{
+		root = recChangeData(data, data2, data3, this.root);
+		}
+	
+	private LinkedListNode<T> recChangeData(T data, T data2, T data3, LinkedListNode<T> tree) {
+		// not finished, should edit data
+		if (tree == null) {
+			tree = new LinkedListNode<T>(data, data2, data3);
+		} else if (data2.compareTo(tree.getData2()) < 0) {
+			tree.setLeft(recAdd(data, data2, data3, tree.getLeft()));
+		} else if (data2.compareTo(tree.getData2()) > 0) {
+			tree.setRight(recAdd(data, data2, data3, tree.getRight()));
+		}
+		return tree;
+	}
+	
 	public void addFull(T data, T data2, T data3, T data4, T data5) {
-		// TODO Auto-generated method stub
+		// receives data from GUI, uses to make a new class in the linked list
 		root = recAddFull(data, data2, data3, data4, data5, this.root);
 		length += 1;
 	}
 
 	private LinkedListNode<T> recAddFull(T data, T data2, T data3, T data4, T data5, LinkedListNode<T> tree) {
-		// TODO Auto-generated method stub
+		// uses recursion to put class in order according to user id.
 		if (tree == null) {
 			tree = new LinkedListNode<T>(data, data2, data3, data4, data5);
-		} else if (data2.compareTo(tree.getData2()) <= 0) {
+		} else if (data2.compareTo(tree.getData2()) < 0) {
 			tree.setLeft(recAddFull(data, data2, data3, data4, data5, tree.getLeft()));
 		} else if (data2.compareTo(tree.getData2()) > 0) {
 			tree.setRight(recAddFull(data, data2, data3, data4, data5, tree.getRight()));
@@ -33,16 +50,17 @@ public class LinkedList<T extends Comparable<T>> {
 	}
 	
 	public void add(T data, T data2, T data3) {
-		// TODO Auto-generated method stub
+		// this method adds items to the linked list that are not full
 		root = recAdd(data, data2, data3, this.root);
 		length += 1;
 	}
 
 	private LinkedListNode<T> recAdd(T data, T data2, T data3, LinkedListNode<T> tree) {
-		// TODO Auto-generated method stub
+		// checks to make sure no duplicate items(based on select data)
+		// uses recursion to check entire list
 		if (tree == null) {
 			tree = new LinkedListNode<T>(data, data2, data3);
-		} else if (data2.compareTo(tree.getData2()) <= 0) {
+		} else if (data2.compareTo(tree.getData2()) < 0) {
 			tree.setLeft(recAdd(data, data2, data3, tree.getLeft()));
 		} else if (data2.compareTo(tree.getData2()) > 0) {
 			tree.setRight(recAdd(data, data2, data3, tree.getRight()));
@@ -50,26 +68,23 @@ public class LinkedList<T extends Comparable<T>> {
 		return tree;
 	}
 
-	public boolean remove(T data) throws EmptyLinkedListException {
-		// TODO Auto-generated method stub
+	public boolean remove(T data2) throws EmptyLinkedListException {
+		// public method that removes information from linked list
+		// this method only passes on information to private methods
 		if (!isEmpty()) {
 			try {
-				root = recRemove(data, this.root);
-			} catch (DataNotFoundException e) {
-				// TODO Auto-generated catch block
-
+				root = recRemove(data2, this.root);
+			} catch (DataNotFoundException e) { 
 			}
 		} else {
 			throw new EmptyLinkedListException("LinkedList is empty!");
 		}
-
-		length -= 1;
 		return found;
 	}
 
 	private LinkedListNode<T> recRemove(T data2, LinkedListNode<T> tree)
 			throws EmptyLinkedListException, DataNotFoundException {
-		// TODO Auto-generated method stub
+		// searches to find data in linked list
 		if (tree == null) {
 			found = false;
 		} else if (data2.compareTo(tree.getData2()) < 0) {
@@ -85,8 +100,8 @@ public class LinkedList<T extends Comparable<T>> {
 
 	}
 
-	private LinkedListNode<T> removeNode(LinkedListNode<T> tree) throws EmptyLinkedListException,
-			DataNotFoundException {
+	private LinkedListNode<T> removeNode(LinkedListNode<T> tree) throws EmptyLinkedListException, DataNotFoundException 
+	{	//once node is found by previous methods, removes node
 		if (tree.getLeft() == null) {
 			return tree.getRight();
 		} else if (tree.getRight() == null) {
@@ -96,73 +111,36 @@ public class LinkedList<T extends Comparable<T>> {
 			tree.setData(temp.getData());
 			tree.setLeft(recRemove(temp.getData(), tree.getLeft()));
 		}
+		length -= 1;
 		return tree;
 	}
 
 	private LinkedListNode<T> findHighestLeft(LinkedListNode<T> tree) {
-		// TODO Auto-generated method stub
+		// gets top of the linked list
 		while (tree.getRight() != null) {
 			tree = tree.getRight();
 		}
 		return tree;
 	}
 
-	public String search(T data2)throws EmptyLinkedListException //searches student info for userID
-		{LinkedListNode<T> trunk = null;
-		String answer ="";
-		if ((!isEmpty())) {
-			try {
-				trunk = recSearch(data2, this.root);
-			} catch (DataNotFoundException e) {
-							}
-		} else {
-			throw new EmptyLinkedListException("LinkedList is empty!");
-		}
-
-				if (found)
-					{answer =trunk.getData()+ " - "+ trunk.getData2();}
-				else 
-					{answer=" Word not present. Would you like to add it?";}
-		return answer;}
-	
-	private LinkedListNode<T> recSearch(T data, LinkedListNode<T> tree) throws EmptyLinkedListException, DataNotFoundException {
-			// TODO Auto-generated method stub
-			if ((tree == null)) {
-				found = false;
-			} else if (data.compareTo(tree.getData()) < 0) {
-				tree.setLeft(recSearch(data, tree.getLeft()));
-				System.out.println("b3");
-			} else if (data.compareTo(tree.getData()) > 0) {
-				tree.setRight(recSearch(data, tree.getRight()));
-				System.out.println("k2");
-				
-			} else {
-				found = true;
-			}
-			
-			return tree;
-		
-	}
-
-	
-	public boolean contains(T data) {
-		return recContains(data, this.root);
+	public boolean contains(T data2) {
+		//returns either True or False based on recContains, searches for userID
+		return recContains(data2, this.root);
 	}
 	
-	private boolean recContains(T data, LinkedListNode<T> root) {
-		// when root is null
+	private boolean recContains(T data2, LinkedListNode<T> root) {
+		// when root is null returns false, otherwise uses recursion to search linked list for data match
 		if (root == null) {
 			return false;
 		}
 
-		if (data.compareTo(root.getData()) == 0) {
+		if (data2.compareTo(root.getData2()) == 0) {
 			return true;
-		} else if (data.compareTo(root.getData()) < 0) {
-			return recContains(data, root.getLeft());
+		} else if (data2.compareTo(root.getData2()) < 0) {
+			return recContains(data2, root.getLeft());
 		} else {
-			return recContains(data, root.getRight());
+			return recContains(data2, root.getRight());
 		}
-		// return false;
 
 	}
 	
